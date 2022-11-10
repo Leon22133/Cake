819d3a25c6ff46f056c29d4513994d6cf10eb771
Leon22133 <isip_l.g.andreev@mpt.ru>
2022-11-10 18:06:50 +0300

    Удалил все гиты и создал тортики

---
+++ Class2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractosNumber5
+{
+    internal class Cake
+    {
+        public int cost;
+        public string forma;
+        public string razmer;
+        public string vkusy;
+        public string Kolichestvo_Korgey;
+        public string glazur;
+        public string ukrashenia;
+        public Cake(int ParamCost, string ParamForma, string ParamSize, string Paramvkus, string ParamKolvo_Korgey, string Paramglazur, string ParamDecor)
+        {
+            cost = ParamCost;
+            forma = ParamForma;
+            razmer = ParamSize;
+            vkusy = Paramvkus;
+            Kolichestvo_Korgey = ParamKolvo_Korgey;
+            glazur = Paramglazur;
+            ukrashenia = ParamDecor;
+        }
+    }
+}
